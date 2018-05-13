909437cb5ea4cbc24fae895881dba60f336ea1ee
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-05-13 14:56:09 +0200

    Better DI factory

--- config/di/http-server.php
+++ config/di/http-server.php
@@ -1,9 +1,13 @@
 <?php
 
+use Psr\Log\LoggerInterface;
+use React\EventLoop\LoopInterface;
 use ReactiveApps\Command\HttpServer\HttpServer;
 
 return [
-    HttpServer::class => \DI\create(HttpServer::class)
-        ->parameter('address', \DI\get('config.http-server.address'))
-        ->parameter('handler', \DI\get('config.http-server.handler')),
+    HttpServer::class => \DI\factory(function (LoopInterface $loop, LoggerInterface $logger, string $address, callable $handler) {
+        return new HttpServer($loop, $logger, $address, $handler);
+    })
+    ->parameter('address', \DI\get('config.http-server.address'))
+    ->parameter('handler', \DI\get('config.http-server.handler')),
 ];
