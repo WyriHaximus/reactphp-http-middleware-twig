f842841ba39aca198e5c7639256c04588142819f
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-05-14 21:28:40 +0200

    Webroot middleware

--- config/di/http-server.php
+++ config/di/http-server.php
@@ -5,9 +5,10 @@ use React\EventLoop\LoopInterface;
 use ReactiveApps\Command\HttpServer\HttpServer;
 
 return [
-    HttpServer::class => \DI\factory(function (LoopInterface $loop, LoggerInterface $logger, string $address, callable $handler) {
-        return new HttpServer($loop, $logger, $address, $handler);
+    HttpServer::class => \DI\factory(function (LoopInterface $loop, LoggerInterface $logger, string $address, callable $handler, string $public = null) {
+        return new HttpServer($loop, $logger, $address, $handler, $public);
     })
     ->parameter('address', \DI\get('config.http-server.address'))
-    ->parameter('handler', \DI\get('config.http-server.handler')),
+    ->parameter('handler', \DI\get('config.http-server.handler'))
+    ->parameter('public', \DI\get('config.http-server.public')),
 ];
--- src/HttpServer.php
+++ src/HttpServer.php
@@ -7,6 +7,7 @@ use React\EventLoop\LoopInterface;
 use React\Http\Server as ReactHttpServer;
 use React\Socket\Server as SocketServer;
 use ReactiveApps\Command\Command;
+use WyriHaximus\React\Http\Middleware\WebrootPreloadMiddleware;
 use WyriHaximus\React\Http\PSR15MiddlewareGroup\Factory;
 
 final class HttpServer implements Command
@@ -34,17 +35,24 @@ final class HttpServer implements Command
     private $handler;
 
     /**
+     * @var string
+     */
+    private $public;
+
+    /**
      * @param LoopInterface $loop
      * @param LoggerInterface $logger
      * @param string $address
      * @param callable $handler
+     * @param string $public
      */
-    public function __construct(LoopInterface $loop, LoggerInterface $logger, string $address, callable $handler)
+    public function __construct(LoopInterface $loop, LoggerInterface $logger, string $address, callable $handler, string $public = null)
     {
         $this->loop = $loop;
         $this->logger = $logger;
         $this->address = $address;
         $this->handler = $handler;
+        $this->public = $public;
     }
 
     public function __invoke()
@@ -52,6 +60,9 @@ final class HttpServer implements Command
         $socket = new SocketServer($this->address, $this->loop);
         $middleware = [];
         $middleware[] = Factory::create($this->loop, $this->logger);
+        if ($this->public !== null && file_exists($this->public) && is_dir($this->public)) {
+            $middleware[] = new WebrootPreloadMiddleware($this->public, $this->logger);
+        }
         $middleware[] = $this->handler;
         $httpServer = new ReactHttpServer($middleware);
         $httpServer->listen($socket);
