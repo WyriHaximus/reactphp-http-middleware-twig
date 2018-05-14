c51fefe4c84c941bad558de58d067e33ee7ad101
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-05-14 22:35:22 +0200

    Use context logger and callable throwable logger to add context to what ever happens in the HTTP server

--- src/HttpServer.php
+++ src/HttpServer.php
@@ -7,6 +7,8 @@ use React\EventLoop\LoopInterface;
 use React\Http\Server as ReactHttpServer;
 use React\Socket\Server as SocketServer;
 use ReactiveApps\Command\Command;
+use WyriHaximus\PSR3\CallableThrowableLogger\CallableThrowableLogger;
+use WyriHaximus\PSR3\ContextLogger\ContextLogger;
 use WyriHaximus\React\Http\Middleware\WebrootPreloadMiddleware;
 use WyriHaximus\React\Http\PSR15MiddlewareGroup\Factory;
 
@@ -49,7 +51,7 @@ final class HttpServer implements Command
     public function __construct(LoopInterface $loop, LoggerInterface $logger, string $address, callable $handler, string $public = null)
     {
         $this->loop = $loop;
-        $this->logger = $logger;
+        $this->logger = new ContextLogger($logger, ['section' => 'http-server'], 'http-server');
         $this->address = $address;
         $this->handler = $handler;
         $this->public = $public;
@@ -61,10 +63,14 @@ final class HttpServer implements Command
         $middleware = [];
         $middleware[] = Factory::create($this->loop, $this->logger);
         if ($this->public !== null && file_exists($this->public) && is_dir($this->public)) {
-            $middleware[] = new WebrootPreloadMiddleware($this->public, $this->logger);
+            $middleware[] = new WebrootPreloadMiddleware(
+                $this->public,
+                new ContextLogger($this->logger, ['section' => 'webroot'], 'webroot')
+            );
         }
         $middleware[] = $this->handler;
         $httpServer = new ReactHttpServer($middleware);
+        $httpServer->on('error', CallableThrowableLogger::create($this->logger));
         $httpServer->listen($socket);
     }
 }
