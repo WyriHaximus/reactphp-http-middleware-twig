2885d8797102f82c9f6e7b6e3c581a3dff562916
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-06-01 08:04:05 +0200

    Optionally add extra middleware before and after our default middleware

--- config/di/http-server.php
+++ config/di/http-server.php
@@ -9,8 +9,18 @@ use WyriHaximus\React\Http\Middleware\WebrootPreloadMiddleware;
 use WyriHaximus\React\Http\PSR15MiddlewareGroup\Factory;
 
 return [
-    HttpServer::class => \DI\factory(function (LoopInterface $loop, LoggerInterface $logger, Shutdown $shutdown, string $address, callable $handler, string $public = null) {
+    HttpServer::class => \DI\factory(function (
+        LoopInterface $loop,
+        LoggerInterface $logger,
+        Shutdown $shutdown,
+        string $address,
+        callable $handler,
+        array $middlwarePrefix = [],
+        array $middlwareSuffix = [],
+        string $public = null
+    ) {
         $middleware = [];
+        array_push($middleware, ...$middlwarePrefix);
         $middleware[] = Factory::create($loop, $logger);
         if ($public !== null && file_exists($public) && is_dir($public)) {
             $middleware[] = new WebrootPreloadMiddleware(
@@ -18,11 +28,14 @@ return [
                 new ContextLogger($logger, ['section' => 'webroot'], 'webroot')
             );
         }
+        array_push($middleware, ...$middlwareSuffix);
         $middleware[] = $handler;
 
         return new HttpServer($loop, $logger, $shutdown, $address, $middleware);
     })
     ->parameter('address', \DI\get('config.http-server.address'))
     ->parameter('handler', \DI\get('config.http-server.handler'))
-    ->parameter('public', \DI\get('config.http-server.public')),
+    ->parameter('public', \DI\get('config.http-server.public'))
+    ->parameter('middlwarePrefix', \DI\get('config.http-server.middleware.prefix'))
+    ->parameter('middlwareSuffix', \DI\get('config.http-server.middleware.suffix')),
 ];
