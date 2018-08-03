456ca6cfe3402793b08e8a00581c8c10f402ae0c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-08-03 07:55:16 +0200

    Configurate child process pool metrics collector in this package

--- config/di/child-process-pool.php
+++ config/di/child-process-pool.php
@@ -4,15 +4,30 @@ use React\EventLoop\LoopInterface;
 use WyriHaximus\React\ChildProcess\Closure\ClosureChild;
 use WyriHaximus\React\ChildProcess\Pool\Factory\Flexible;
 use WyriHaximus\React\ChildProcess\Pool\Options;
+use WyriHaximus\React\ChildProcess\Pool\PoolInterface;
+use WyriHaximus\React\Inspector\ChildProcessPools\ChildProcessPoolsCollector;
 
 return [
     'internal.http-server.child-process.pool' => \DI\factory(function (
-        LoopInterface $loop
+        LoopInterface $loop,
+        ChildProcessPoolsCollector $collector = null
     ) {
-        return Flexible::createFromClass(
+        $childProcessPool = Flexible::createFromClass(
             ClosureChild::class,
             $loop,
-            [Options::TTL => 0.25, Options::MIN_SIZE => 0, Options::MAX_SIZE => 5]
+            [
+                Options::TTL      => 0.25,
+                Options::MIN_SIZE => 0,
+                Options::MAX_SIZE => 5,
+            ]
         );
+
+        if ($collector instanceof ChildProcessPoolsCollector) {
+            $childProcessPool->done(function (PoolInterface $childProcessPool) use ($collector) {
+                $collector->register('http-server', $childProcessPool);
+            });
+        }
+
+        return $childProcessPool;
     }),
 ];
