071be68dfc47edf9939c3154deee8018c6f5e316
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-04-17 22:49:46 +0200

    Functional TemplateRenderMiddleware
    Does expect a Twig\Environment to be available in the container

--- src/Middleware/TemplateRenderMiddleware.php
+++ src/Middleware/TemplateRenderMiddleware.php
@@ -2,12 +2,48 @@
 
 namespace ReactiveApps\Command\HttpServer\Middleware;
 
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use function React\Promise\resolve;
+use ReactiveApps\Command\HttpServer\TemplateResponse;
+use function RingCentral\Psr7\stream_for;
+use Twig\Environment;
 
 final class TemplateRenderMiddleware
 {
+    /** @var Environment */
+    private $twig;
+
+    /**
+     * TemplateRenderMiddleware constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     public function __invoke(ServerRequestInterface $request, $next)
     {
-        return $next($request);
+        $template = $request->getAttribute('request-handler-template', false);
+
+        if ($template === false) {
+            return $next($request);
+        }
+
+        return resolve($next($request))->then(function (ResponseInterface $response) use ($template) {
+            if ($response instanceof TemplateResponse) {
+                $response = $response->withBody(
+                    stream_for(
+                        $this->twig->render(
+                            $template,
+                            $response->getTemplateData()
+                        )
+                    )
+                );
+            }
+
+            return $response;
+        });
     }
 }
--- tests/Middleware/TemplateRenderMiddlewareTest.php
+++ tests/Middleware/TemplateRenderMiddlewareTest.php
@@ -4,15 +4,37 @@ namespace ReactiveApps\Tests\Command\HttpServer\Middleware;
 
 use Psr\Http\Message\ResponseInterface;
 use ReactiveApps\Command\HttpServer\Middleware\TemplateRenderMiddleware;
+use ReactiveApps\Command\HttpServer\TemplateResponse;
 use RingCentral\Psr7\Response;
 use RingCentral\Psr7\ServerRequest;
-use WyriHaximus\TestUtilities\TestCase;
+use Twig\Environment;
+use Twig\Loader\ArrayLoader;
+use WyriHaximus\AsyncTestUtilities\AsyncTestCase;
 
 /**
  * @internal
  */
-final class TemplateRenderMiddlewareTest extends TestCase
+final class TemplateRenderMiddlewareTest extends AsyncTestCase
 {
+    public function testRenderOnTemplateResponse(): void
+    {
+        $request = (new ServerRequest(
+            'GET',
+            'https://example.com/'
+        ))->withAttribute('request-handler-template', 'template_pawufhuiwfe');
+
+        /** @var ResponseInterface $response */
+        $response = $this->await((new TemplateRenderMiddleware(new Environment(
+            new ArrayLoader([
+                'template_pawufhuiwfe' => 'Beer from a {{ foo }}',
+            ])
+        )))($request, function () {
+            return (new TemplateResponse())->withTemplateData(['foo' => 'bar']);
+        }));
+
+        self::assertSame('Beer from a bar', $response->getBody()->getContents());
+    }
+
     public function testCallNextOnNonTemplateResponse(): void
     {
         $request = (new ServerRequest(
@@ -21,7 +43,9 @@ final class TemplateRenderMiddlewareTest extends TestCase
         ));
 
         /** @var ResponseInterface $response */
-        $response = (new TemplateRenderMiddleware())($request, function () {
+        $response = (new TemplateRenderMiddleware(new Environment(
+            new ArrayLoader([])
+        )))($request, function () {
             return new Response(200, [], 'no-template');
         });
 
