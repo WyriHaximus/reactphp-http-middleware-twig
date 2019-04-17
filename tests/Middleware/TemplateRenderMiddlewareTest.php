934d8a159bbf6d88e06cf64c1176f01054e20eec
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-04-17 21:53:41 +0200

    TemplateRenderMiddleware should ignore all non-TemplateReponse's

--- tests/Middleware/TemplateRenderMiddlewareTest.php
+++ tests/Middleware/TemplateRenderMiddlewareTest.php
@@ -2,7 +2,9 @@
 
 namespace ReactiveApps\Tests\Command\HttpServer\Middleware;
 
+use Psr\Http\Message\ResponseInterface;
 use ReactiveApps\Command\HttpServer\Middleware\TemplateRenderMiddleware;
+use RingCentral\Psr7\Response;
 use RingCentral\Psr7\ServerRequest;
 use WyriHaximus\TestUtilities\TestCase;
 
@@ -11,18 +13,18 @@ use WyriHaximus\TestUtilities\TestCase;
  */
 final class TemplateRenderMiddlewareTest extends TestCase
 {
-    public function testCallNext(): void
+    public function testCallNextOnNonTemplateResponse(): void
     {
         $request = (new ServerRequest(
             'GET',
             'https://example.com/'
         ));
 
-        $handlerCalled = false;
-        (new TemplateRenderMiddleware())($request, function () use (&$handlerCalled): void {
-            $handlerCalled = true;
+        /** @var ResponseInterface $response */
+        $response = (new TemplateRenderMiddleware())($request, function () {
+            return new Response(200, [], 'no-template');
         });
 
-        self::assertTrue($handlerCalled);
+        self::assertSame('no-template', $response->getBody()->getContents());
     }
 }
