f78c5a0cdacae4e756753d9f3c21001f2b7b41f1
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-04-25 22:54:05 +0200

    Corrected docblock body type

--- src/JsonResponse.php
+++ src/JsonResponse.php
@@ -8,11 +8,11 @@ use WyriHaximus\React\Stream\Json\JsonStream;
 final class JsonResponse
 {
     /**
-     * @param int    $status  Status code for the response, if any.
-     * @param array  $headers Headers for the response, if any.
-     * @param mixed  $body    Stream body.
-     * @param string $version Protocol version.
-     * @param string $reason  Reason phrase (a default will be used if possible).
+     * @param int        $status  Status code for the response, if any.
+     * @param array      $headers Headers for the response, if any.
+     * @param JsonStream $body    Stream body.
+     * @param string     $version Protocol version.
+     * @param string     $reason  Reason phrase (a default will be used if possible).
      */
     public static function create(
         $status = 200,
