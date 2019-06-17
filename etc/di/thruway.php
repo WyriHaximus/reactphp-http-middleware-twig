ca3f499541228040976818ec8fafd7b75092fda2
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-06-17 11:57:25 +0200

    Heartbeat RPC on heartbeat realm

--- etc/di/thruway.php
+++ etc/di/thruway.php
@@ -5,7 +5,10 @@ use Psr\Log\LoggerInterface;
 use React\EventLoop\LoopInterface;
 use ReactiveApps\Command\HttpServer\Event\RealmClose;
 use ReactiveApps\Command\HttpServer\Event\RealmOpen;
+use ReactiveApps\Command\HttpServer\Rpc\Heartbeat;
 use ReactiveApps\Command\HttpServer\Thruway\Realm;
+use ReactiveApps\Command\HttpServer\Thruway\RealmAuth;
+use ReactiveApps\Command\HttpServer\Thruway\Rpc;
 use Thruway\ClientSession;
 use Thruway\Logging\Logger;
 use Thruway\Middleware;
@@ -40,11 +43,33 @@ return [
 
         return new Middleware(['/'], $loop, $router);
     })
-        ->parameter('realms', \DI\get('config.http-server.thruway.realms')),
+        ->parameter('realms', \DI\get('internal.http-server.thruway.realms')),
     JWT::class => \DI\factory(function (
         array $realms = []
     ) {
         return new JWT($realms);
+    })
+        ->parameter('realms', \DI\get('internal.http-server.thruway.realms')),
+    'internal.http-server.thruway.realms' => \DI\factory(function (
+        array $realms = []
+    ) {
+        $realms[] = new Realm(
+            'heartbeat',
+            new RealmAuth(false, ''),
+            [
+                new Rpc('heartbeat', Heartbeat::class),
+            ]
+        );
+
+        $realms = \array_values($realms);
+
+        $indexRealms = [];
+        /** @var Realm $realm */
+        foreach ($realms as $realm) {
+            $indexRealms[$realm->getName()] = $realm;
+        }
+
+        return $indexRealms;
     })
         ->parameter('realms', \DI\get('config.http-server.thruway.realms')),
 ];
